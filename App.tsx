198b236848e42df26812dd4359a61d3cfd52336e
Luis Felipe G. Garcia <lfelipeggarcia@gmail.com>
2023-05-29 21:50:42 -0300

    feat: add screens Login and SignUp

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/Screens/Login';
+import SignUp from './src/Screens/SignUp';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Login} />
+        <Stack.Screen name='SignUp' component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/Screens/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function Login() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login</Text>
+    </View>
+  );
+}
+
+export default Login;
