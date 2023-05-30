21bf29f1420e0c34110300653b27aa69e023818e
Luis Felipe G. Garcia <lfelipeggarcia@gmail.com>
2023-05-29 22:08:45 -0300

    feat: Screen Home

---
+++ src/Screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View, Button } from 'react-native';
+
+function Home({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+export default Home;
